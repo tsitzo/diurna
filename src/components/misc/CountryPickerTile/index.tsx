abd4c27beae588ed755a46bf24c046c02c9bcea9
Federico Giorgino <federicog.dev@gmail.com>
2021-10-24 23:12:05 +0200

    CountryPickerTile

---
+++ src/components/misc/CountryPickerTile/index.tsx
@@ -0,0 +1,42 @@
+import { useTheme } from "@react-navigation/native";
+import React, { FC, useContext } from "react";
+import { View, Image, TouchableOpacity } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+
+import Typography from "../../text/Typography";
+
+import { CountryContext } from "../../../context/Country.context";
+import { Country } from "../../../types/types";
+import { styles } from "./styles";
+
+interface ICountryPickerTileProps {
+  country: Country;
+}
+
+const CountryPickerTile: FC<ICountryPickerTileProps> = ({ country }) => {
+  const { colors } = useTheme();
+  const { selectedCountry, selectCountry } = useContext(CountryContext);
+
+  return (
+    <TouchableOpacity onPress={() => selectCountry(country.iso)}>
+      <View style={styles.row}>
+        <View style={styles.leftContainer}>
+          <Image source={{ uri: country.flag }} style={styles.flag} />
+          <Typography>{country.name}</Typography>
+        </View>
+
+        {selectedCountry === country.iso && (
+          <View>
+            <Ionicons
+              name="md-checkmark-sharp"
+              size={18}
+              color={colors.primary}
+            />
+          </View>
+        )}
+      </View>
+    </TouchableOpacity>
+  );
+};
+
+export default CountryPickerTile;
---
+++ src/components/misc/CountryPickerTile/styles.tsx
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  row: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  flag: { height: 45, width: 45, marginRight: 15 },
+  leftContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  countryText: { fontWeight: "700" },
+});
