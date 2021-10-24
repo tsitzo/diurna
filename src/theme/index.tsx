e6b91ec93c7ce539cc53eace380ca5db34be1717
Federico Giorgino <federicog.dev@gmail.com>
2021-10-24 18:06:31 +0200

    Initial CustomTheme setup

---
+++ src/theme/index.tsx
@@ -0,0 +1,39 @@
+import {
+  DefaultTheme,
+  DarkTheme,
+  ExtendedTheme,
+} from "@react-navigation/native";
+
+export const CustomLightTheme: ExtendedTheme = {
+  dark: false,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: "rgb(216, 11, 140)",
+    secondary: "rgb(0, 174, 239)",
+    tertiary: "rgb(34, 31, 114)",
+    danger: "rgb(208, 2, 27)",
+    background: "rgb(239, 238, 244)",
+    card: "rgb(255, 255, 255)",
+    text: "rgb(0, 0, 0)",
+    subtext: "rgb(102, 102, 102)",
+    separator: "rgb(194, 194, 195)",
+    highlight: "rgb(199, 198, 203)",
+  },
+};
+
+export const CustomDarkTheme: ExtendedTheme = {
+  dark: true,
+  colors: {
+    ...DarkTheme.colors,
+    primary: "rgb(216, 11, 140)",
+    secondary: "rgb(0, 174, 239)",
+    tertiary: "rgb(34, 31, 114)",
+    danger: "rgb(208, 2, 27)",
+    background: "rgb(239, 238, 244)",
+    card: "rgb(255, 255, 255)",
+    text: "rgb(0, 0, 0)",
+    subtext: "rgb(102, 102, 102)",
+    separator: "rgb(194, 194, 195)",
+    highlight: "rgb(199, 198, 203)",
+  },
+};
---
+++ src/types/*.d.ts
@@ -0,0 +1,23 @@
+import "@react-navigation/native";
+
+// Override the theme in react native navigation to accept our custom theme props.
+declare module "@react-navigation/native" {
+  export type ExtendedTheme = {
+    dark: boolean;
+    colors: {
+      primary: string;
+      secondary: string;
+      tertiary: string;
+      danger: string;
+      background: string;
+      card: string;
+      text: string;
+      subtext: string;
+      separator: string;
+      border: string;
+      highlight: string;
+      notification: string;
+    };
+  };
+  export function useTheme(): ExtendedTheme;
+}
