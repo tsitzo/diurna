172ca99566bd382004b81eda5648d477a6fad0b7
Federico Giorgino <federicog.dev@gmail.com>
2021-10-27 00:14:04 +0200

    Settings Screen Layout

--- src/screens/Main/SettingsScreen/index.tsx
+++ src/screens/Main/SettingsScreen/index.tsx
@@ -1,13 +1,63 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, { useContext } from "react";
+import {
+  View,
+  Text,
+  ScrollView,
+  Switch,
+  Platform,
+  TouchableOpacity,
+} from "react-native";
+import { Entypo } from "@expo/vector-icons";
+
+import SafeArea from "../../../components/layout/SafeArea";
+import Spacer from "../../../components/layout/Spacer";
+import Typography from "../../../components/text/Typography";
+
+import { ThemeContext } from "../../../context/Theme.context";
+import { MainStackNavProps } from "../../../navigation/MainStack";
 
 import { styles } from "./styles";
+import { useTheme } from "@react-navigation/native";
 
-const SettingsScreen = () => {
+const SettingsScreen = ({ navigation }: MainStackNavProps<"MainTabs">) => {
+  const { isDarkTheme, switchTheme } = useContext(ThemeContext);
+  const { colors } = useTheme();
   return (
-    <View>
-      <Text></Text>
-    </View>
+    <SafeArea>
+      <ScrollView style={styles.scrollWrapper}>
+        <Spacer y={20} />
+
+        <TouchableOpacity style={styles.row}>
+          <Typography>Select Country</Typography>
+          <Entypo name="chevron-right" size={24} color={colors.text} />
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.row}>
+          <Typography>Select Categories</Typography>
+          <Entypo name="chevron-right" size={24} color={colors.text} />
+        </TouchableOpacity>
+        <View style={styles.row}>
+          <Typography>Dark Mode</Typography>
+          <Switch
+            value={isDarkTheme}
+            onValueChange={switchTheme}
+            trackColor={{ true: colors.primary, false: colors.border }}
+          />
+        </View>
+        <Spacer y={20} />
+
+        <TouchableOpacity style={styles.row}>
+          <Typography>Bookmarked Articles</Typography>
+          <Entypo name="chevron-right" size={24} color={colors.text} />
+        </TouchableOpacity>
+
+        <Spacer y={20} />
+
+        <TouchableOpacity style={styles.row}>
+          <Typography>About</Typography>
+          <Entypo name="chevron-right" size={24} color={colors.text} />
+        </TouchableOpacity>
+      </ScrollView>
+    </SafeArea>
   );
 };
 
--- src/screens/Main/SettingsScreen/styles.ts
+++ src/screens/Main/SettingsScreen/styles.ts
@@ -1,3 +1,11 @@
 import { StyleSheet } from "react-native";
 
-export const styles = StyleSheet.create({});
+export const styles = StyleSheet.create({
+  row: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginVertical: 15,
+  },
+  scrollWrapper: { paddingHorizontal: 17 },
+});
